916c1616fbd4cc292725b631963483410ad72072
Do-hyun-Kim <dohyun8032@gmail.com>
2022-05-17 21:15:30 +0900

    Product Json 파일 추가 or Swift Demo App init

---
+++ Swift-E-Commerce-Demo/Swift-E-Commerce-DemoUITests/Swift_E_Commerce_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swift_E_Commerce_DemoUITestsLaunchTests.swift
+//  Swift-E-Commerce-DemoUITests
+//
+//  Created by Kim dohyun on 2022/05/17.
+//
+
+import XCTest
+
+class Swift_E_Commerce_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
